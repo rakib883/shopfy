3a0e392cc0b53030440a9516ae27709dd6c39298
rakib883 <sheikhrakib883@gmail.com>
2024-10-20 09:29:49 +0600

    push

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Slider from "../components/slider/slider"
-import Trending from "../components/trendingarea/Trending";
+import Slider from "../components/slider"
+import Trending from "../components/trendingArea/Trending";
 import Catagory from "../components/catagory/Catagory"
 import Faceleties from "../components/faceleties/Faceleties"
 import Banner from "../components/adsBanner/Banner"
 import Eletronic from "../components/eletronic/Eletronic"
 import BottomSlider from "../components/bottomSlider/BottomSlider"
-import NewAraval from "../components/newarrival/NewAraval"
+import NewAraval from "../components/newArrival/NewAraval"
 import Feture from "../components/fetureArea/Feture";
 function page() {
   return (
